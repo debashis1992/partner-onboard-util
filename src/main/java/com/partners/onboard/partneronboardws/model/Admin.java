5642b28a91fb0535b4351dd3bbaf613b5444ea8e
debashis1992 <d2011das@gmail.com>
2024-03-02 15:33:00 +0530

    adding code

--- src/main/java/com/partners/onboard/partneronboardws/model/Admin.java
+++ src/main/java/com/partners/onboard/partneronboardws/model/Admin.java
@@ -1,4 +1,14 @@
 package com.partners.onboard.partneronboardws.model;
 
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
 public class Admin implements ModuleUser {
+    String id;
+
+    public Admin() {
+        id = UUID.randomUUID().toString();
+    }
 }
